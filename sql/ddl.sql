b0adb4acc5964c0540fbc28c15b44724b708e6f9
yonguk <wldyddnr@gmail.com>
2023-04-23 23:10:15 +0900

    활동칼로리계산기 데이터베이스 추가

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -5,3 +5,18 @@ create table member(
     password varchar(50) not null,
     email varchar(50) not null
 );
+
+drop table if exists metabolism CASCADE;
+create table metabolism(
+    mid varchar(20) primary key,
+    age varchar(10) not null,
+    height varchar(10) not null,
+    weight varchar(10) not null,
+    sex varchar(10) not null,
+    activity varchar(10) not null,
+    basal_metabolic_rate varchar(10) not null,
+    activity_metabolism varchar(10) not null
+    );
+
+
+)
