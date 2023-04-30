83163b7f5f044e19f8ad1fb692b9d2f5c622dacc
yonguk <wldyddnr@gmail.com>
2023-04-30 22:18:19 +0900

    metabolism db수정

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -12,7 +12,7 @@ create table metabolism(
     age varchar(10) not null,
     height varchar(10) not null,
     weight varchar(10) not null,
-    sex varchar(10) not null,
+    gender varchar(10) not null,
     activity varchar(10) not null,
     basal_metabolic_rate varchar(10) not null,
     activity_metabolism varchar(10) not null
