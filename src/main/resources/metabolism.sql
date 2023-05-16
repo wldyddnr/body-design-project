382662614527d25a63351f4d1a6c25e6b09eaaa3
yonguk <wldyddnr@gmail.com>
2023-05-17 01:14:40 +0900

    sql 분리

--- src/main/resources/metabolism.sql
+++ src/main/resources/metabolism.sql
@@ -1,11 +1,3 @@
-drop table if exists member CASCADE;
-create table member(
-    id varchar(50) primary key,
-    name varchar(50) not null,
-    password varchar(50) not null,
-    email varchar(50) not null
-);
-
 drop table if exists metabolism CASCADE;
 create table metabolism(
     mid varchar(20) primary key,
